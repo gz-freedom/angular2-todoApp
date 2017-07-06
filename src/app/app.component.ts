4340cd2b88114f69d1e8cb9f58f42ca38b36c0b4
gz-freedom <myec2005@163.com>
2017-07-06 15:49:42 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Todo } from "./todo";
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'app works!';
+  //newTodo: Todo = new Todo();
+  constructor(private todoDataService: TodoDataService) {}
+
+  // addTodo() {
+  //   this.todoDataService.addTodo(this.newTodo);
+  //   this.newTodo = new Todo();
+  // }
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
+  onToggleTodoComplet(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  onRemoveTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  getTodos() {
+    this.todoDataService.getAllTodos();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent,
+    TodoListFooterComponent,
+    TodoListComponent,
+    TodoListItemComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
